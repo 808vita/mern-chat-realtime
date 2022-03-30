d7835ca90aa7ab1f1569e3b070fedc1dd62e7d7f
Sudharsun <psu.fabulo@gmail.com>
2022-03-30 20:17:48 +0530

    login -fix

--- frontend/src/Pages/components/Authentication/Login.js
+++ frontend/src/Pages/components/Authentication/Login.js
@@ -12,12 +12,60 @@ import { VStack } from "@chakra-ui/layout";
 const Login = () => {
 	const [show, setShow] = useState(false);
 	const handleClick = () => setShow(!show);
-
+	const toast = useToast();
+	const history = useHistory();
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
-	const [loading, setLoading] = useState(false);
 
-	const submitHandler = () => {};
+	const submitHandler = async () => {
+		if (!email || !password) {
+			toast({
+				title: "Please Fill all the Feilds",
+				status: "warning",
+				duration: 5000,
+				isClosable: true,
+				position: "bottom",
+			});
+
+			return;
+		}
+
+		// console.log(email, password);
+		try {
+			const config = {
+				headers: {
+					"Content-type": "application/json",
+				},
+			};
+
+			const { data } = await axios.post(
+				"/api/user/login",
+				{ email, password },
+				config
+			);
+
+			// console.log(JSON.stringify(data));
+			toast({
+				title: "Login Successful",
+				status: "success",
+				duration: 5000,
+				isClosable: true,
+				position: "bottom",
+			});
+			localStorage.setItem("userInfo", JSON.stringify(data));
+
+			history.push("/chats");
+		} catch (error) {
+			toast({
+				title: "Error Occured!",
+				description: error.response.data.message,
+				status: "error",
+				duration: 5000,
+				isClosable: true,
+				position: "bottom",
+			});
+		}
+	};
 
 	return (
 		<VStack spacing="10px">
@@ -51,7 +99,6 @@ const Login = () => {
 				width="100%"
 				style={{ marginTop: 15 }}
 				onClick={submitHandler}
-				isLoading={loading}
 			>
 				Login
 			</Button>
