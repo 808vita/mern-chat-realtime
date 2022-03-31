f7b4cf8a88043b3c391f919a579e1288bf9eea3e
Sudharsun <psu.fabulo@gmail.com>
2022-03-31 09:12:31 +0530

    message routes controller

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
