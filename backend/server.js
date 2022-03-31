a9defca460714849b1a883d0b9015a80ee3f789e
Sudharsun <psu.fabulo@gmail.com>
2022-03-31 19:03:01 +0530

    oof

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
 	console.log(`server stated at port ${PORT}`);
