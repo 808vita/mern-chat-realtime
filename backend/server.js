be029aa8f892ef13a2f48f9db5e0bc0aab1d271d
Sudharsun <psu.fabulo@gmail.com>
2022-03-31 13:31:55 +0530

    start backed socket

--- backend/server.js
+++ backend/server.js
@@ -48,4 +48,9 @@ io.on("connection", (socket) => {
 		socket.join(userData._id);
 		socket.emit("connected");
 	});
+
+	socket.on("join chat", (room) => {
+		socket.join(room);
+		console.log("user joined room:" + room);
+	});
 });
