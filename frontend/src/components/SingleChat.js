cfd3cf7e0008823b9254da5f824a61695f8d9714
Sudharsun <psu.fabulo@gmail.com>
2022-03-31 20:13:09 +0530

    oof

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://chatzzz.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://chatzzz-mern.herokuapp.com/"; // "https://chatzzz-mern.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
